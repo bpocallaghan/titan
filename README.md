e8cd2b877c27cd897ba44619dc2c3b8eb66ad912
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2016-04-07 09:25:24 +0100

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# titan
+# Titan
+
 Useful classes used for every Laravel project
+--under development--
+
+###Want to see the current package in action, have a look at my starter project.
+####[Laravel Starter Project](https://github.com/bpocallaghan/laravel-admin-starter)
+
+#### Documentation is in progress
+
+# What it includes
+
+### Generate unique Slug
+
+\Models\Traits\SlugUnique.php
+Generate a unique slug.
+It will suffix "-index" if slug is already in use
+
+\Models\Traits\SlugUniqueModels.php
+Create a list of models.
+This will create a unique slug from all those models.
+
+## Installation
+
+## TODO
+
+- Documentation
+- Cleanup code
+
+## Note
+
+Please keep in mind this is for my personal workflow and might not fit your need.
+I developed this to help speed up my day to day workflow. 
+Please let me know about any issues or if you have any suggestions.
+
+## My other Packages
+
+- [Flash a bootstrap alert](https://github.com/bpocallaghan/alert)
+- [Laravel Notifications with icons and animations and with a timeout](https://github.com/bpocallaghan/notify)
+- [Laravel custom Generate Files with a config file and publishable stubs](https://github.com/bpocallaghan/generators)
---
+++ src/Models/Traits/SlugUniqueModels.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Models\BodyStyle;
+use App\Models\Category;
+use App\Models\Color;
+use App\Models\Condition;
+use App\Models\Exterior;
+use App\Models\FuelType;
+use App\Models\Interior;
+use App\Models\Lifestyle;
+use App\Models\Make;
+use App\Models\Modell;
+use App\Models\SellerType;
+use App\Models\Transmission;
+use ReflectionClass;
+
+trait SlugUniqueModels
+{
+    // list of models to look to make unique slug
+    protected $models = [
+        Category::class,
+        Make::class,
+        Modell::class,
+        Condition::class,
+        Transmission::class,
+        Color::class,
+        FuelType::class,
+        Lifestyle::class,
+        Interior::class,
+        Exterior::class,
+        BodyStyle::class,
+        SellerType::class,
+    ];
+
+    function setSlugAttribute($slug)
+    {
+        $this->attributes['slug'] = $this->makeSlugUnique($slug);
+    }
+
+    /**
+     * Make Slug Unique in same table
+     * @param $slug
+     * @return string
+     */
+    private function makeSlugUnique($slug)
+    {
+        $slug = str_slug($slug);
+
+        // check updating
+        $slugUpdate = $this->checkUpdatingSlug($slug);
+
+        if ($slugUpdate !== false) {
+            return $slugUpdate;
+        }
+
+        // get existing slugs
+        $list = $this->getExistingSlugs($slug);
+
+        if ($list->count() === 0) {
+            return $slug;
+        }
+
+        // generate unique suffix
+        return $this->generateSuffix($slug, $list);
+    }
+
+    /**
+     * Get existing slugs matching slug
+     *
+     * @param $slug
+     * @return \Illuminate\Support\Collection|static
+     */
+    private function getExistingSlugs($slug)
+    {
+        $list = collect();
+        foreach ($this->models as $class) {
+
+            // get entries matching slug
+            $slugs = $this->eloqent($class)
+                ->whereRaw("slug LIKE '$slug%'")
+                ->withTrashed()// trashed, when entry gets activated
+                ->get()
+                ->pluck('slug');
+
+            $list = $list->merge($slugs);
+        }
+
+        return $list;
+    }
+
+    /**
+     * Suffix unique index to slug
+     *
+     * @param $slug
+     * @param $list
+     * @return string
+     */
+    private function generateSuffix($slug, $list)
+    {
+        // loop through list and get highest index number
+        $index = $list->map(function ($s) use ($slug) {
+            // str_replace instead of explode('-');
+            return intval(str_replace($slug . '-', '', $s));
+        })->sort()->last();
+
+        return $slug . '-' . ($index + 1);
+    }
+
+    /**
+     * Get the Eloquent Class
+     * @param $class
+     * @return object
+     */
+    private function eloqent($class)
+    {
+        return (new ReflectionClass($class))->newInstanceArgs();
+    }
+
+    /**
+     * Check if we are updating
+     * Find entries with same slug
+     * Exlude current model's entry
+     *
+     * @param $slug
+     * @return bool
+     */
+    private function checkUpdatingSlug($slug)
+    {
+        if ($this->id >= 1) {
+
+            $found = false;
+            foreach ($this->models as $class) {
+
+                // find entries matching slug, exclude updating entry
+                $builder = $this->eloqent($class)->where('slug', $slug);
+
+                // same model - exlude check
+                if ($class == self::class) {
+                    $builder = $builder->where('id', '!=', $this->id);
+                }
+
+                // found same slug - break, need new slug
+                if ($found = $builder->first()) {
+                    break;
+                }
+            }
+
+            // save to use current slug for update
+            if ($found == false || $found == null) {
+                return $slug;
+            }
+        }
+
+        // new unique slug needed
+        return false;
+    }
+}
