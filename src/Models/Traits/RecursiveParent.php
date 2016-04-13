811561418a27bf1d54ab249ef7b82a76a3e0c69e
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2016-04-13 07:40:24 +0100

    recursive parent - add 2 static helper functions (when creating item, list parents in html select)

--- src/Models/Traits/RecursiveParent.php
+++ src/Models/Traits/RecursiveParent.php
@@ -5,6 +5,27 @@ namespace Titan\Models\Traits;
 trait RecursiveParent
 {
     /**
+     * Get the top level parents
+     * If the parent_id is null or parent_id = 0
+     * @return mixed
+     */
+    public static function parents()
+    {
+        return self::whereRaw('(parent_id is NULL OR parent_id = 0)')
+            ->orderBy('title')
+            ->get();
+    }
+
+    /**
+     * Get the parents as list (for html selects)
+     * @return mixed
+     */
+    public static function parentsList()
+    {
+        return self::parents()->pluck('title', 'id')->toArray();
+    }
+
+    /**
      * Get the parent
      * @return \Eloquent
      */
