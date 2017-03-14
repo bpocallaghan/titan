aa954e0402e256af302dd92a60b8a00dda460062
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2017-03-14 17:56:31 +0200

    Analytics - Add referers, active vistors, user gender, user age, interests, devices, device category

--- src/Controllers/Traits/Analytics.php
+++ src/Controllers/Traits/Analytics.php
@@ -35,6 +35,16 @@ trait Analytics
             'pointHighlightFill'   => "#fff",
             'pointHighlightStroke' => "rgba(220, 220, 220, 1)",
             'data'                 => [],
+        ],
+        [
+            'label'                => "",
+            'fillColor'            => "rgba(60, 141, 188, 0.1)",
+            'strokeColor'          => "rgba(60, 141, 188, 1)",
+            'pointColor'           => "#3b8bba",
+            'pointStrokeColor'     => "rgba(60,141,188,1)",
+            'pointHighlightFill'   => "#fff",
+            'pointHighlightStroke' => "rgba(220, 220, 220, 1)",
+            'data'                 => [],
         ]
     ];
 
@@ -75,7 +85,7 @@ trait Analytics
      */
     public function getVisitors()
     {
-        return $this->monthlySummary('ga:users');
+        return $this->monthlyComparison('ga:users');
     }
 
     /**
@@ -84,7 +94,7 @@ trait Analytics
      */
     public function getUniqueVisitors()
     {
-        return $this->monthlySummary('ga:newUsers');
+        return $this->monthlyComparison('ga:newUsers');
     }
 
     /**
@@ -93,7 +103,7 @@ trait Analytics
      */
     public function getBounceRate()
     {
-        return $this->monthlySummary('ga:bounceRate');
+        return $this->monthlyComparison('ga:bounceRate');
     }
 
     /**
@@ -102,7 +112,7 @@ trait Analytics
      */
     public function getAvgPageLoad()
     {
-        return $this->monthlySummary('ga:avgPageLoadTime');
+        return json_response($this->monthlySummary('ga:avgPageLoadTime'));
     }
 
     /**
@@ -113,10 +123,33 @@ trait Analytics
     {
         $dates = $this->getStartEndDate();
 
-        $items = LaravelAnalytics::getTopKeyWordsForPeriod($dates['start'],
-            $dates['end']);
+        $items = LaravelAnalytics::getTopKeyWordsForPeriod($dates['start'], $dates['end']);
 
         return $items;
+    }
+
+    /**
+     * Get the top referrers for duration
+     * @return \Spatie\LaravelAnalytics\Collection
+     */
+    public function getReferrers()
+    {
+        $dates = $this->getStartEndDate();
+
+        $items = LaravelAnalytics::getTopReferrersForPeriod($dates['start'], $dates['end'], 30);
+
+        return $items;
+    }
+
+    /**
+     * Get the active users currently viewing the website
+     * @return int
+     */
+    public function getActiveVisitors()
+    {
+        $total = LaravelAnalytics::getActiveUsers();
+
+        return json_response($total);
     }
 
     /**
@@ -128,8 +161,7 @@ trait Analytics
     {
         $dates = $this->getStartEndDate();
 
-        $data = LaravelAnalytics::getVisitorsAndPageViewsForPeriod($dates['start'],
-            $dates['end']);
+        $data = LaravelAnalytics::getVisitorsAndPageViewsForPeriod($dates['start'], $dates['end']);
 
         $totalViews = ['labels' => []];
         $visitors = [];
@@ -155,8 +187,7 @@ trait Analytics
     {
         $dates = $this->getStartEndDate();
 
-        $items = LaravelAnalytics::getMostVisitedPagesForPeriod($dates['start'],
-            $dates['end']);
+        $items = LaravelAnalytics::getMostVisitedPagesForPeriod($dates['start'], $dates['end']);
 
         return $items;
     }
@@ -170,8 +201,8 @@ trait Analytics
     {
         $dates = $this->getStartEndDate();
 
-        $data = LaravelAnalytics::getTopBrowsersForPeriod($dates['start'], $dates['end'],
-            7)->toArray();
+        $data = LaravelAnalytics::getTopBrowsersForPeriod($dates['start'], $dates['end'], 7)
+            ->toArray();
 
         $items = [];
         shuffle($data); // shuffle results / randomimize chart color sections
@@ -183,22 +214,181 @@ trait Analytics
     }
 
     /**
-     * Helper to get the months analytics
-     * @param string $metrics
-     * @return \Illuminate\Http\JsonResponse|int
+     * Get the gender comparisons
+     * @return array
      */
-    private function monthlySummary($metrics = 'ga:users')
+    public function getUsersGender()
     {
-        $end = Carbon::now();
-        $start = Carbon::now()->startOfMonth();
+        $dates = $this->getStartEndDate();
 
-        $data = LaravelAnalytics::performQuery($start, $end, $metrics);
+        $data = LaravelAnalytics::performQuery($dates['start'], $dates['end'], 'ga:sessions',
+            ['dimensions' => 'ga:userGender']);
 
-        if ($data->rows && count($data->rows) >= 1 && count($data->rows[0]) >= 1) {
-            return json_response($data->rows[0][0]);
+        $items = [];
+        $rows = $data->rows;
+        shuffle($rows); // shuffle results / randomimize chart color sections
+        foreach ($rows as $k => $item) {
+            $items[] = $this->getPieDataSet(ucfirst($item[0]), $item[1]);
         }
 
-        return 'n/a';
+        return $items;
+    }
+
+    /**
+     * Get the users' age comparisons
+     * @return array
+     */
+    public function getUsersAge()
+    {
+        $dates = $this->getStartEndDate();
+
+        $data = LaravelAnalytics::performQuery($dates['start'], $dates['end'], 'ga:sessions',
+            ['dimensions' => 'ga:userAgeBracket']);
+
+        $labels = [];
+        $datasets = [];
+        $rows = $data->rows;
+        foreach ($rows as $k => $item) {
+            $labels[] = ucfirst($item[0]);
+            $datasets[] = $item[1];
+        }
+
+        $datasets = [$this->getDataSet('Ages', $datasets, 0)];
+
+        return ['labels' => $labels, 'datasets' => $datasets];
+    }
+
+    /**
+     * Get the the users interests - affinity
+     * @return \Spatie\LaravelAnalytics\Collection
+     */
+    public function getInterestsAffinity()
+    {
+        return $this->getInterests('ga:interestAffinityCategory');
+    }
+
+    /**
+     * Get the the users interests - market
+     * @return \Spatie\LaravelAnalytics\Collection
+     */
+    public function getInterestsMarket()
+    {
+        return $this->getInterests('ga:interestInMarketCategory');
+    }
+
+    /**
+     * Get the the users interests - affinity
+     * @return \Spatie\LaravelAnalytics\Collection
+     */
+    public function getInterestsOther()
+    {
+        return $this->getInterests('ga:interestOtherCategory');
+    }
+
+    /**
+     * Get all the devices by sessions
+     * @return mixed
+     */
+    public function getDevices()
+    {
+        $dates = $this->getStartEndDate();
+
+        $data = LaravelAnalytics::performQuery($dates['start'], $dates['end'], 'ga:sessions', [
+            'dimensions'  => 'ga:mobileDeviceInfo',
+            'sort'        => '-ga:sessions',
+            'max-results' => 30
+        ]);
+
+        return $data->rows;
+    }
+
+    /**
+     * Get the desktop vs mobile vs tablet
+     */
+    public function getDeviceCategory()
+    {
+        $dates = $this->getStartEndDate();
+
+        $data = LaravelAnalytics::performQuery($dates['start'], $dates['end'], 'ga:sessions', [
+            'dimensions' => 'ga:deviceCategory'
+        ]);
+
+        $items = [];
+        foreach ($data->rows as $k => $item) {
+            $items[] = $this->getPieDataSet(ucfirst($item[0]), $item[1]);
+        }
+
+        return $items;
+    }
+
+    /**
+     * Get the the users interests
+     * @param $dimensions
+     * @return \Spatie\LaravelAnalytics\Collection
+     */
+    public function getInterests($dimensions)
+    {
+        $dates = $this->getStartEndDate();
+
+        $data = LaravelAnalytics::performQuery($dates['start'], $dates['end'], 'ga:sessions', [
+            'dimensions'  => $dimensions,
+            'sort'        => '-ga:sessions',
+            'max-results' => 30
+        ]);
+
+        return $data->rows;
+    }
+
+    /**
+     * Helper to get the months analytics
+     * @param string $metrics
+     * @param string $month
+     * @return \Illuminate\Http\JsonResponse|int
+     */
+    private function monthlySummary($metrics = 'ga:users', $month = 'month')
+    {
+        if ($month == '-month_1') {
+            $end = Carbon::now()->subMonth()->endOfMonth();
+            $start = Carbon::now()->subMonth()->startOfMonth();
+        }
+        else {
+            $end = Carbon::now();
+            $start = Carbon::now()->startOfMonth();
+        }
+
+        $data = LaravelAnalytics::performQuery($start->startOfDay(), $end->endOfDay(), $metrics);
+
+        if ($data->rows && count($data->rows) >= 1 && count($data->rows[0]) >= 1) {
+            return $data->rows[0][0];
+        }
+
+        return 0;
+    }
+
+    /**
+     * Get this and last month of the metrics for a comparison
+     * @param string $metrics
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function monthlyComparison($metrics = 'ga:users')
+    {
+        $thisMonth = $this->monthlySummary($metrics);
+        $lastMonth = $this->monthlySummary($metrics, '-month_1');
+
+        return json_response([
+            'month'      => [
+                'value'     => $thisMonth,
+                'color'     => "#00c0ef",
+                'highlight' => "#00a7d0",
+                'label'     => "Current"
+            ],
+            'last_month' => [
+                'value'     => $lastMonth,
+                'color'     => "#00a65a",
+                'highlight' => "#008d4c",
+                'label'     => "Previous"
+            ]
+        ]);
     }
 
     /**
@@ -244,8 +434,12 @@ trait Analytics
      * @param int $index
      * @return mixed
      */
-    private function getPieDataSet($label, $data, $index = 0)
+    private function getPieDataSet($label, $data, $index = -1)
     {
+        if ($index < 0) {
+            $index = rand(0, count($this->pieData) - 1);
+        }
+
         $set = $this->pieData[$index];
         $set['label'] = $label;
         $set['value'] = $data;
