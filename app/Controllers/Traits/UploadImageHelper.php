9ab614799be1ff2ac79e26e9e0ab74fbef02fe1d
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2019-01-29 08:57:09 +0200

    Traits - Upload image and document

---
+++ app/Controllers/Traits/UploadImageHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bpocallaghan\Titan\Http\Controllers\Traits;
+
+use Image;
+
+trait UploadImageHelper
+{
+    /**
+     * Upload the banner image, create a thumb as well
+     *
+     * @param        $file
+     * @param string $path
+     * @param array  $size
+     * @return string|void
+     */
+    protected function uploadImage(
+        $file, $path = '', $size = ['o' => [1920, 500], 'tn' => [576, 150]]
+    ) {
+        $name = token();
+        $extension = $file->guessClientExtension();
+
+        $filename = $name . '.' . $extension;
+        $filenameThumb = $name . '-tn.' . $extension;
+        $imageTmp = Image::make($file->getRealPath());
+
+        if (!$imageTmp) {
+            return notify()->error('Oops', 'Something went wrong', 'warning shake animated');
+        }
+
+        $path = upload_path_images($path);
+
+        // original
+        $imageTmp->save($path . $name . '-o.' . $extension);
+
+        // save the image
+        $image = $imageTmp->fit($size['o'][0], $size['o'][1])->save($path . $filename);
+
+        $image->fit($size['tn'][0], $size['tn'][1])->save($path . $filenameThumb);
+
+        return $filename;
+    }
+}
