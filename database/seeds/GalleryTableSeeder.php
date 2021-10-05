4d5ce8b4ecebe67feb30f863a2a5142bb517b1d3
Claudiu Brinduse <brinduse.claudiu@gmail.com>
2021-10-05 14:20:36 +0300

    add check in for

--- database/seeds/GalleryTableSeeder.php
+++ database/seeds/GalleryTableSeeder.php
@@ -17,5 +17,25 @@ class GalleryTableSeeder extends Seeder
             ]);
         }
 
+
+        for ($i = 0; $i < 20; $i++) {
+            $item = Article::create([
+                'title'       => $faker->sentence(3),
+                'content'     => "<p>{$faker->paragraph(3)}</p>",
+                'active_from' => $faker->dateTimeBetween('-5 weeks', '-1 weeks')->format('Y-m-d'),
+                //'active_to' => $faker->dateTimeBetween('+5 weeks')
+                'category_id' => $faker->numberBetween(1, 5),
+                'slug'        => 'asd'
+            ]);
+
+            for ($a = 0; $a < $faker->numberBetween(2, 4); $a++) {
+                $r = random_int(1, 2);
+                $item->photos()->create([
+                    'name'     => $faker->sentence(2),
+                    'filename' => "news-{$r}.jpg",
+                ]);
+            }
+        }
+
     }
 }
