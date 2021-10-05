95511b45ac81b2edf863a55cfe0bccfc13a70967
Claudiu Brinduse <brinduse.claudiu@gmail.com>
2021-10-05 14:02:50 +0300

    Add GalleryTableSeeder

---
+++ database/seeds/GalleryTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+namespace Bpocallaghan\Titan\Seeds;
+use Bpocallaghan\Titan\Models\ArticleCategory;
+use Illuminate\Database\Seeder;
+use Bpocallaghan\Titan\Models\Article;
+
+class GalleryTableSeeder extends Seeder
+{
+    public function run(Faker\Generator $faker)
+    {
+        Article::truncate();
+        ArticleCategory::truncate();
+        
+    }
+}
